4307891ee1a89bda55bcd37313e2d4c49626f294
Nikki Elisha Plaza <nikkiplaza24@gmail.com>
2024-10-03 19:35:52 +0800

    Added paddle's measurements and color

--- PY_Pong_Game.py
+++ PY_Pong_Game.py
@@ -12,13 +12,17 @@ WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 
 class Paddle:
+    COLOR = WHITE
+
     def __init__(self, x, y, width, height):
         self.x = x
         self.y = y
         self.width = width
         self.height = height
 
-    
+    def draw(self, win):
+        pygame.draw.rectangle(win, self.COLOR, (self.x, self.y, self.width, self.height))
+        
 
 def draw(win):
     win.fill(BLACK)
