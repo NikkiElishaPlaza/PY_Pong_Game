edeea3cf1999edd201f3a5f7e31ad1b3d144a1b4
Nikki Elisha Plaza <nikkiplaza24@gmail.com>
2024-10-03 23:46:41 +0800

    Added paddles' key movements

--- PY_Pong_Game.py
+++ PY_Pong_Game.py
@@ -43,7 +43,15 @@ def draw(win, paddles):
     pygame.display.update()
 
 def handle_paddle_movement(keys, left_paddle, right_paddle):
-    
+    if keys[pygame.K_w]:
+        left_paddle.move(up=True)
+    if keys[pygame.K_s]:
+        left_paddle.move(up=False)
+
+    if keys[pygame.K_UP]:
+        right_paddle.move(up=True)
+    if keys[pygame.K_DOWN]:
+        right_paddle.move(up=False)
 
 def main():
     run = True
