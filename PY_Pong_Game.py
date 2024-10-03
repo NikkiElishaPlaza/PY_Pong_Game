21edf01f7f933be0ddb7bf35b1d53e350196baff
Nikki Elisha Plaza <nikkiplaza24@gmail.com>
2024-10-03 19:14:50 +0800

    Added speed (fps) for the game

--- PY_Pong_Game.py
+++ PY_Pong_Game.py
@@ -6,11 +6,15 @@ WIDTH, HEIGHT, = 700, 500
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("PONG GAME")
 
+FPS = 60
 
 def main():
     run = True
+    clock = pygame.time.Clock()
 
     while run:
+        clock.tick(FPS)
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
