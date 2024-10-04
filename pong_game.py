72531376861c308de9c2f73db996968a13463cb9
Nikki Elisha Plaza <nikkiplaza24@gmail.com>
2024-10-04 18:22:31 +0800

    updated game scoring

--- pong_game.py
+++ pong_game.py
@@ -60,6 +60,12 @@ class Ball:
 def draw(win, paddles, ball, left_score, right_score):
     win.fill(BLACK)
 
+    left_score_text = SCORE_FONT.render(f"{left_score}", 1, WHITE)
+    right_score_text = SCORE_FONT.render(f"{right_score}", 1, WHITE)
+    win.blit(left_score_text, (WIDTH//4 - left_score_text.get_width()//2, 20))
+    win.blit(right_score_text, (WIDTH * (3/4) - right_score_text.get_width()//2, 20))
+
+
     for paddle in paddles:
         paddle.draw(win)
 
@@ -70,6 +76,8 @@ def draw(win, paddles, ball, left_score, right_score):
 
     ball.draw(win)
     pygame.display.update()
+
+
 
 def handle_collision(ball, left_paddle, right_paddle):
     if ball.y + ball.radius >= HEIGHT:
